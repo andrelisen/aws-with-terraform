86f579005852bf9ade07f4fcfc0947795c403860
Andrelise Nunes Lemos Pinheiro <andrelise.pinheiro@compasso.com.br>
2024-05-15 11:20:05 -0300

    Tutorial variables

--- 01-terraform-basico/01-primeiro-script/main.tf
+++ 01-terraform-basico/01-primeiro-script/main.tf
@@ -1,13 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "terraform-pessoal"
 }
 
 resource "aws_s3_bucket" "my-bucket-test" {
-  bucket = "bucket-aws-com-terraform-14052024"
+  bucket = "bucket-aws-com-terraform-15052024"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Nome              = "Bucket AWS Com Terraform"
+    CriadoPor         = "Andrelise"
+    UltimaAtualizacao = "2024-05-15"
   }
 }
---
+++ 01-terraform-basico/02-variaveis/variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region"{
+    type = string
+    description = ""
+    default = "us-east-1"
+}
+
+variable "aws_profile"{
+    type = string
+    description = ""
+    default = "terraform-pessoal"
+}
+
+variable "instance_ami"{
+    type = string
+    description = ""
+    default = "ami-0bb84b8ffd87024d8"
+}
+
+variable "instance_type"{
+    type = string
+    description = ""
+    default = "t3.micro"
+}
+
+variable "tags"{
+    type = map(string)
+    description = ""
+    default = {
+        Name = "Ubuntu"
+        Project = "Curso AWS Com Terraform"
+    }
+}
