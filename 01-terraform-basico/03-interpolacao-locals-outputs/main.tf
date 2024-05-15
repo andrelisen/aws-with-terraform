0babdda24b9a7e690891b948663c6083cbc3d3fb
Andrelise Nunes Lemos Pinheiro <andrelise.pinheiro@compasso.com.br>
2024-05-15 13:55:50 -0300

    update required provider main.tf

--- 01-terraform-basico/03-interpolacao-locals-outputs/main.tf
+++ 01-terraform-basico/03-interpolacao-locals-outputs/main.tf
@@ -11,10 +11,3 @@ provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
 }
-
-resource "aws_instance" "web" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-
-  tags = var.tags
-}
